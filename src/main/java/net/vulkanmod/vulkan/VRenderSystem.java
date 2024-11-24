8553f97e3467bc0667bd2897668fb065bc492a7c
ItsWinterBush <jade26mine@gmail.com>
2024-11-24 14:31:17 +0800

    Update VRenderSystem.java

--- src/main/java/net/vulkanmod/vulkan/VRenderSystem.java
+++ src/main/java/net/vulkanmod/vulkan/VRenderSystem.java
@@ -2,44 +2,43 @@ package net.vulkanmod.vulkan;
 
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.platform.Window;
-
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.BufferBuilder;
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
+import com.mojang.blaze3d.vertex.Tesselator;
+import com.mojang.blaze3d.vertex.VertexFormat;
 import net.minecraft.client.Minecraft;
-import net.vulkanmod.vulkan.device.DeviceManager;
+import net.minecraft.client.renderer.GameRenderer;
 import net.vulkanmod.vulkan.shader.PipelineState;
-import net.vulkanmod.vulkan.util.ColorUtil;
 import net.vulkanmod.vulkan.util.MappedBuffer;
 import net.vulkanmod.vulkan.util.VUtil;
-import org.joml.Matrix4f;
-import org.lwjgl.opengl.GL11;
+import com.mojang.math.Matrix4f;
+import org.joml.Vector2f;
 import org.lwjgl.system.MemoryUtil;
-
-import static org.lwjgl.vulkan.VK10.*;
+import org.spongepowered.asm.mixin.Overwrite;
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
 
 import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 
-public abstract class VRenderSystem {
-    private static final float DEFAULT_DEPTH_VALUE = 1.0f;
+public class VRenderSystem {
 
+    private static String capsString = "";
+    private static String cpuInfo;
+    
     private static long window;
 
     public static boolean depthTest = true;
     public static boolean depthMask = true;
     public static int depthFun = 515;
-    public static int topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
-    public static int polygonMode = VK_POLYGON_MODE_FILL;
-    public static boolean canSetLineWidth = false;
 
     public static int colorMask = PipelineState.ColorMask.getColorMask(true, true, true, true);
 
     public static boolean cull = true;
 
-    public static boolean logicOp = false;
-    public static int logicOpFun = 0;
-
-    public static float clearDepthValue = DEFAULT_DEPTH_VALUE;
-    public static FloatBuffer clearColor = MemoryUtil.memCallocFloat(4);
+    public static float clearDepth = 1.0f;
+    public static FloatBuffer clearColor = MemoryUtil.memAllocFloat(4);
 
     public static MappedBuffer modelViewMatrix = new MappedBuffer(16 * 4);
     public static MappedBuffer projectionMatrix = new MappedBuffer(16 * 4);
@@ -55,36 +54,77 @@ public abstract class VRenderSystem {
 
     public static MappedBuffer screenSize = new MappedBuffer(2 * 4);
 
-    public static float alphaCutout = 0.0f;
-
     private static final float[] depthBias = new float[2];
 
-    public static void initRenderer() {
+    public static void initRenderer()
+    {
+        RenderSystem.assertInInitPhase();
+
+        try {
+            CentralProcessor centralprocessor = (new SystemInfo()).getHardware().getProcessor();
+            cpuInfo = String.format("%dx %s", centralprocessor.getLogicalProcessorCount(), centralprocessor.getProcessorIdentifier().getName()).replaceAll("\\s+", " ");
+        } catch (Throwable throwable) {
+        }
+
         Vulkan.initVulkan(window);
+
     }
 
-    public static MappedBuffer getScreenSize() {
-        updateScreenSize();
-        return screenSize;
-    }
-
-    public static void updateScreenSize() {
-        Window window = Minecraft.getInstance().getWindow();
-
-        screenSize.putFloat(0, (float) window.getWidth());
-        screenSize.putFloat(4, (float) window.getHeight());
-    }
-
-    public static void setWindow(long window) {
-        VRenderSystem.window = window;
-    }
-
-    public static ByteBuffer getChunkOffset() {
-        return ChunkOffset.buffer;
-    }
+    public static ByteBuffer getChunkOffset() { return ChunkOffset.buffer; }
 
     public static int maxSupportedTextureSize() {
-        return DeviceManager.deviceProperties.limits().maxImageDimension2D();
+        return Vulkan.deviceProperties.limits().maxImageDimension2D();
+    }
+
+    public static void renderCrosshair(int p_69348_, boolean p_69349_, boolean p_69350_, boolean p_69351_) {
+        RenderSystem.assertOnRenderThread();
+//        GlStateManager._disableTexture();
+//        GlStateManager._depthMask(false);
+//        GlStateManager._disableCull();
+        VRenderSystem.depthMask(false);
+        RenderSystem.setShader(GameRenderer::getRendertypeLinesShader);
+        Tesselator tesselator = RenderSystem.renderThreadTesselator();
+        BufferBuilder bufferbuilder = tesselator.getBuilder();
+        RenderSystem.lineWidth(4.0F);
+        bufferbuilder.begin(VertexFormat.Mode.LINES, DefaultVertexFormat.POSITION_COLOR_NORMAL);
+        if (p_69349_) {
+            bufferbuilder.vertex(0.0D, 0.0D, 0.0D).color(0, 0, 0, 255).normal(1.0F, 0.0F, 0.0F).endVertex();
+            bufferbuilder.vertex((double)p_69348_, 0.0D, 0.0D).color(0, 0, 0, 255).normal(1.0F, 0.0F, 0.0F).endVertex();
+        }
+
+        if (p_69350_) {
+            bufferbuilder.vertex(0.0D, 0.0D, 0.0D).color(0, 0, 0, 255).normal(0.0F, 1.0F, 0.0F).endVertex();
+            bufferbuilder.vertex(0.0D, (double)p_69348_, 0.0D).color(0, 0, 0, 255).normal(0.0F, 1.0F, 0.0F).endVertex();
+        }
+
+        if (p_69351_) {
+            bufferbuilder.vertex(0.0D, 0.0D, 0.0D).color(0, 0, 0, 255).normal(0.0F, 0.0F, 1.0F).endVertex();
+            bufferbuilder.vertex(0.0D, 0.0D, (double)p_69348_).color(0, 0, 0, 255).normal(0.0F, 0.0F, 1.0F).endVertex();
+        }
+
+        tesselator.end();
+        RenderSystem.lineWidth(2.0F);
+        bufferbuilder.begin(VertexFormat.Mode.LINES, DefaultVertexFormat.POSITION_COLOR_NORMAL);
+        if (p_69349_) {
+            bufferbuilder.vertex(0.0D, 0.0D, 0.0D).color(255, 0, 0, 255).normal(1.0F, 0.0F, 0.0F).endVertex();
+            bufferbuilder.vertex((double)p_69348_, 0.0D, 0.0D).color(255, 0, 0, 255).normal(1.0F, 0.0F, 0.0F).endVertex();
+        }
+
+        if (p_69350_) {
+            bufferbuilder.vertex(0.0D, 0.0D, 0.0D).color(0, 255, 0, 255).normal(0.0F, 1.0F, 0.0F).endVertex();
+            bufferbuilder.vertex(0.0D, (double)p_69348_, 0.0D).color(0, 255, 0, 255).normal(0.0F, 1.0F, 0.0F).endVertex();
+        }
+
+        if (p_69351_) {
+            bufferbuilder.vertex(0.0D, 0.0D, 0.0D).color(127, 127, 255, 255).normal(0.0F, 0.0F, 1.0F).endVertex();
+            bufferbuilder.vertex(0.0D, 0.0D, (double)p_69348_).color(127, 127, 255, 255).normal(0.0F, 0.0F, 1.0F).endVertex();
+        }
+
+        tesselator.end();
+        RenderSystem.lineWidth(1.0F);
+//        GlStateManager._enableCull();
+        RenderSystem.depthMask(true);
+//        GlStateManager._enableTexture();
     }
 
     public static void applyMVP(Matrix4f MV, Matrix4f P) {
@@ -94,12 +134,12 @@ public abstract class VRenderSystem {
     }
 
     public static void applyModelViewMatrix(Matrix4f mat) {
-        mat.get(modelViewMatrix.buffer.asFloatBuffer());
+        mat.store(modelViewMatrix.buffer.asFloatBuffer());
         //MemoryUtil.memPutFloat(MemoryUtil.memAddress(modelViewMatrix), 1);
     }
 
     public static void applyProjectionMatrix(Matrix4f mat) {
-        mat.get(projectionMatrix.buffer.asFloatBuffer());
+        mat.store(projectionMatrix.buffer.asFloatBuffer());
     }
 
     public static void calculateMVP() {
@@ -110,7 +150,7 @@ public abstract class VRenderSystem {
     }
 
     public static void setTextureMatrix(Matrix4f mat) {
-        mat.get(TextureMatrix.buffer.asFloatBuffer());
+        mat.store(TextureMatrix.buffer.asFloatBuffer());
     }
 
     public static MappedBuffer getTextureMatrix() {
@@ -137,11 +177,17 @@ public abstract class VRenderSystem {
     }
 
     public static void setShaderColor(float f1, float f2, float f3, float f4) {
-        ColorUtil.setRGBA_Buffer(shaderColor, f1, f2, f3, f4);
+        shaderColor.putFloat(0, f1);
+        shaderColor.putFloat(4, f2);
+        shaderColor.putFloat(8, f3);
+        shaderColor.putFloat(12, f4);
     }
 
     public static void setShaderFogColor(float f1, float f2, float f3, float f4) {
-        ColorUtil.setRGBA_Buffer(shaderFogColor, f1, f2, f3, f4);
+        shaderFogColor.putFloat(0, f1);
+        shaderFogColor.putFloat(4, f2);
+        shaderFogColor.putFloat(8, f3);
+        shaderFogColor.putFloat(12, f4);
     }
 
     public static MappedBuffer getShaderColor() {
@@ -152,19 +198,29 @@ public abstract class VRenderSystem {
         return shaderFogColor;
     }
 
-    public static void setClearColor(float f1, float f2, float f3, float f4) {
-        ColorUtil.setRGBA_Buffer(clearColor, f1, f2, f3, f4);
+    public static void enableColorLogicOp() {
+        Drawer.currentLogicOpState = new PipelineState.LogicOpState(true, 0);
     }
 
-    public static void clear(int mask) {
-        Renderer.clearAttachments(mask);
+    public static void disableColorLogicOp() {
+        Drawer.currentLogicOpState = PipelineState.DEFAULT_LOGICOP_STATE;
     }
 
-    public static void clearDepth(double depth) {
-        clearDepthValue = (float) depth;
+    public static void logicOp(GlStateManager.LogicOp p_69836_) {
+        Drawer.currentLogicOpState.setLogicOp(p_69836_);
     }
 
-    // Pipeline state
+    public static void clearColor(float f1, float f2, float f3, float f4) {
+        clearColor.put(0, f1);
+        clearColor.put(1, f2);
+        clearColor.put(2, f3);
+        clearColor.put(3, f4);
+
+    }
+
+    public static void clear(int v) {
+        Drawer.clearAttachments(v);
+    }
 
     public static void disableDepthTest() {
         depthTest = false;
@@ -174,27 +230,8 @@ public abstract class VRenderSystem {
         depthMask = b;
     }
 
-    public static void setPrimitiveTopologyGL(final int mode) {
-        VRenderSystem.topology = switch (mode) {
-            case GL11.GL_LINES, GL11.GL_LINE_STRIP  -> VK_PRIMITIVE_TOPOLOGY_LINE_LIST;
-            case GL11.GL_TRIANGLE_FAN, GL11.GL_TRIANGLES, GL11.GL_TRIANGLE_STRIP -> VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
-            default -> throw new RuntimeException(String.format("Unknown GL primitive topology: %s", mode));
-        };
-    }
-
-    public static void setPolygonModeGL(final int mode) {
-        VRenderSystem.polygonMode = switch (mode) {
-            case GL11.GL_POINT -> VK_POLYGON_MODE_POINT;
-            case GL11.GL_LINE -> VK_POLYGON_MODE_LINE;
-            case GL11.GL_FILL -> VK_POLYGON_MODE_FILL;
-            default -> throw new RuntimeException(String.format("Unknown GL polygon mode: %s", mode));
-        };
-    }
-
-    public static void setLineWidth(final float width) {
-        if (canSetLineWidth) {
-            Renderer.setLineWidth(width);
-        }
+    public static PipelineState.DepthState getDepthState() {
+        return new PipelineState.DepthState(depthTest, depthMask, depthFun);
     }
 
     public static void colorMask(boolean b, boolean b1, boolean b2, boolean b3) {
@@ -208,53 +245,13 @@ public abstract class VRenderSystem {
     public static void enableDepthTest() {
         depthTest = true;
     }
-
+    
     public static void enableCull() {
         cull = true;
     }
-
+    
     public static void disableCull() {
         cull = false;
-    }
-
-    public static void depthFunc(int depthFun) {
-        VRenderSystem.depthFun = depthFun;
-    }
-
-    public static void enableBlend() {
-        PipelineState.blendInfo.enabled = true;
-    }
-
-    public static void disableBlend() {
-        PipelineState.blendInfo.enabled = false;
-    }
-
-    public static void blendFunc(GlStateManager.SourceFactor sourceFactor, GlStateManager.DestFactor destFactor) {
-        PipelineState.blendInfo.setBlendFunction(sourceFactor, destFactor);
-    }
-
-    public static void blendFunc(int srcFactor, int dstFactor) {
-        PipelineState.blendInfo.setBlendFunction(srcFactor, dstFactor);
-    }
-
-    public static void blendFuncSeparate(GlStateManager.SourceFactor p_69417_, GlStateManager.DestFactor p_69418_, GlStateManager.SourceFactor p_69419_, GlStateManager.DestFactor p_69420_) {
-        PipelineState.blendInfo.setBlendFuncSeparate(p_69417_, p_69418_, p_69419_, p_69420_);
-    }
-
-    public static void blendFuncSeparate(int srcFactorRGB, int dstFactorRGB, int srcFactorAlpha, int dstFactorAlpha) {
-        PipelineState.blendInfo.setBlendFuncSeparate(srcFactorRGB, dstFactorRGB, srcFactorAlpha, dstFactorAlpha);
-    }
-
-    public static void enableColorLogicOp() {
-        logicOp = true;
-    }
-
-    public static void disableColorLogicOp() {
-        logicOp = false;
-    }
-
-    public static void logicOp(GlStateManager.LogicOp logicOp) {
-        logicOpFun = logicOp.value;
     }
 
     public static void polygonOffset(float v, float v1) {
@@ -263,11 +260,54 @@ public abstract class VRenderSystem {
     }
 
     public static void enablePolygonOffset() {
-        Renderer.setDepthBias(depthBias[0], depthBias[1]);
+        Drawer.setDepthBias(depthBias[0], depthBias[1]);
     }
 
     public static void disablePolygonOffset() {
-        Renderer.setDepthBias(0.0F, 0.0F);
+        Drawer.setDepthBias(0.0F, 0.0F);
     }
 
-}
+    public static MappedBuffer getScreenSize() {
+        updateScreenSize();
+        return screenSize;
+    }
+
+    public static void updateScreenSize() {
+        Window window = Minecraft.getInstance().getWindow();
+
+        screenSize.putFloat(0, (float)window.getWidth());
+        screenSize.putFloat(4, (float)window.getHeight());
+    }
+    
+    public static void setWindow(long window) {
+        VRenderSystem.window = window;
+    }
+
+    public static void depthFunc(int p_69457_) {
+        depthFun = p_69457_;
+    }
+
+    public static void enableBlend() {
+        Drawer.blendInfo.enabled = true;
+    }
+
+    public static void disableBlend() {
+        Drawer.blendInfo.enabled = false;
+    }
+
+    public static void blendFunc(GlStateManager.SourceFactor sourceFactor, GlStateManager.DestFactor destFactor) {
+        Drawer.blendInfo.setBlendFunction(sourceFactor, destFactor);
+    }
+
+    public static void blendFunc(int srcFactor, int dstFactor) {
+        Drawer.blendInfo.setBlendFunction(srcFactor, dstFactor);
+    }
+
+    public static void blendFuncSeparate(GlStateManager.SourceFactor p_69417_, GlStateManager.DestFactor p_69418_, GlStateManager.SourceFactor p_69419_, GlStateManager.DestFactor p_69420_) {
+        Drawer.blendInfo.setBlendFuncSeparate(p_69417_, p_69418_, p_69419_, p_69420_);
+    }
+
+    public static void blendFuncSeparate(int srcFactorRGB, int dstFactorRGB, int srcFactorAlpha, int dstFactorAlpha) {
+        Drawer.blendInfo.setBlendFuncSeparate(srcFactorRGB, dstFactorRGB, srcFactorAlpha, dstFactorAlpha);
+    }
+    }
